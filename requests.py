9a436cee3196db8a26d554dac0eb99006c0c44ea
irfantousif <68069389+irfantousif@users.noreply.github.com>
2020-07-22 13:23:20 +0530

    Add files via upload

---
+++ requests.py
@@ -0,0 +1,7 @@
+#$this cide is used to scrape a websites code
+#this is completely legel
+#this is correct according to right to konwledge law 
+import requests
+r=requests.get("give you url")
+print(r)
+print(r.text)
